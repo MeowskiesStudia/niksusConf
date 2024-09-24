47f8522f572579efe4bbdcefe863a12d1cae50f1
Morgan Hand <night.dragonjr@outlook.com>
2024-09-24 13:35:10 -0400

    actually fixed said alacritty config, I think

--- apps/general/alacritty.nix
+++ apps/general/alacritty.nix
@@ -1,18 +1,21 @@
 { config, pkgs, ... }:
 
 {
-  programs.alacritty.settings = {
-    keyboard.bindings = [
-      {
-	key = "C";
-	mods = "Control";
-	action = "Copy";
-      }
-      {
-	key = "V";
-	mods = "Control";
-	action = "Paste";
-      }
-    ];
+  programs.alacritty = {
+    enable = true;
+    settings = {
+      keyboard.bindings = [
+       {
+	  key = "C";
+	  mods = "Control";
+	  action = "Copy";
+	}
+	{
+	  key = "V";
+	  mods = "Control";
+	  action = "Paste";
+	}
+      ];
+    };
   };
 }
