e5314811730d85462baf72f200aafe0adbe72d18
Morgan Hand <night.dragonjr@outlook.com>
2024-09-25 18:22:14 -0400

    temp error fix for swayfx config

--- apps/desktop/swayfx/default.nix
+++ apps/desktop/swayfx/default.nix
@@ -10,32 +10,32 @@ in
     package = pkgs.swayfx;
     #checkConfig = false;
 
-    extraConfig = ''
-      corner_radius 20
-      gaps outer 6
-      gaps inner 3
-    '';
+    #extraConfig = ''
+    #  corner_radius 20
+    #  gaps outer 6
+    #  gaps inner 3
+    #'';
 
-    config = {
-      modifier = super;
-      input = {
-        "type:keyboard" = {
-          xkb_layout = "de";
-          xkb_variant = "nodeadkeys";
-        };
-      };
-      keybindings = {
-        "--to-code ${super}+Return" = "exec ${terminal}";
-      };
-    };
+    #config = {
+    #  modifier = super;
+    #  input = {
+    #    "type:keyboard" = {
+    #      xkb_layout = "de";
+    #      xkb_variant = "nodeadkeys";
+    #    };
+    #  };
+    #  keybindings = {
+    #    "--to-code ${super}+Return" = "exec ${terminal}";
+    #  };
+    #};
 
-    gaps = {
-      outer = 6;
-      inner = 3;
-    };
+    #gaps = {
+    #  outer = 6;
+    #  inner = 3;
+    #};
 
-    window = {
-      titlebar = false;
-    };
+    #window = {
+    #  titlebar = false;
+    #};
   };
 }
