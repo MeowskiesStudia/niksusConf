b150a03eb9259d771530e2b48d7e61367ab8ec06
Morgan Hand <night.dragonjr@outlook.com>
2024-09-24 20:22:18 -0400

    fixed strange error with plasma6

--- apps/desktop/default.nix
+++ apps/desktop/default.nix
@@ -15,10 +15,8 @@
   programs.fish.enable = true;
 
   services = {
-    displayManager = {
-      sddm.enable = true;
-      plasma.enable = true;
-    };
+    displayManager.sddm.enable = true;
+    desktopManager.plasma6.enable = true;
 
     xserver.enable = true;
 
